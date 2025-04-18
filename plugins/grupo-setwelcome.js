966752f0c698e13724bc183a97a6e7d51c60d0e7
SEƗSĦƗRO⚔ṈȺGƗ.il <seishironagi.rukasproyect@gmail.com>
2025-04-18 06:15:06 -0500

    Actualizar grupo-setwelcome.js

--- plugins/grupo-setwelcome.js
+++ plugins/grupo-setwelcome.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, text, isRowner }) => {
 handler.help = ['setwelcome'];
 handler.tags = ['tools'];
 handler.command = ['setwelcome'];
-handler.owner = false;
+handler.rowner = true;
 handler.admin = true;
 
 export default handler;
