a2a68d299ee46c1cde78d0990e4da9910f5d372c
SEƗSĦƗRO⚔ṈȺGƗ.il <seishironagi.rukasproyect@gmail.com>
2025-04-18 06:16:58 -0500

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -59,7 +59,7 @@ global.moneda = 'Dolares'
 //global.welcom1 = '❍ Edita Con El Comando setwelcome'
 //global.welcom2 = '❍ Edita Con El Comando setbye'
 global.banner = 'https://files.catbox.moe/pzgyx3.jpg'
-global.avatar = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742678797993.jpeg'
+global.avatar = 'https://files.catbox.moe/pzgyx3.jpg'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
